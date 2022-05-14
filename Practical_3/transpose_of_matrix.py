67970d02b7bd733963ade983a961d78427a3a3ff
Anas Sain <anassain13@gmail.com>
2022-05-14 12:03:48 +0530

    practical-3 completed

---
+++ Practical_3/transpose_of_matrix.py
@@ -0,0 +1,27 @@
+# Transpose of a Input matrix
+r = int(input("Enter number of rows : "))
+c = int(input("Enter number of cols : "))
+matrix = []
+for i in range(r):
+    temp = [] * c
+    print("Enter row ",i+1)
+    temp = list(map(int,input().split()))
+    matrix.append(temp)
+print("Input matrix is")
+for i in range(r):
+    for j in range(c):
+        print(matrix[i][j] , end = " ")
+    print()
+transpose = [[0 for x in range(r)]for y in range(c)]
+print(transpose)
+for i in range(c):
+    for j in range(r):
+        transpose[i][j] = matrix[j][i]
+
+print("Transpose of matrix is ")
+for i in range(c):
+    for j in range(r):
+        print(transpose[i][j] , end = " ")
+    print()
+
+
